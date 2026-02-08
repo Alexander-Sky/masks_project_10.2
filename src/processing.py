a95d38b9e75f538612a9db3111343f67f2012d0a
Alexander Schischkin <schischkin2@yandex.com>
2026-02-09 00:58:05 +0100

    change imports

--- src/processing.py
+++ src/processing.py
@@ -1,41 +1,27 @@
-from src.processing import filter_by_state, sort_by_date
+from typing import List, Dict
 
-# Тестовые данные
-test_data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
 
-# Тесты для filter_by_state
-def test_filter_executed():
-    result = filter_by_state(test_data)
-    assert len(result) == 2
-    assert all(item['state'] == 'EXECUTED' for item in result)
+def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+    """
+    Фильтрует список операций по статусу
 
-def test_filter_canceled():
-    result = filter_by_state(test_data, 'CANCELED')
-    assert len(result) == 2
-    assert all(item['state'] == 'CANCELED' for item in result)
+    :param data: список словарей с операциями
+    :param state: значение состояния для фильтрации (по умолчанию 'EXECUTED')
+    :return: отфильтрованный список операций
+    """
+    return [item for item in data if item.get('state') == state]
 
-# Тесты для sort_by_date
-def test_sort_desc():
-    result = sort_by_date(test_data)
-    expected_order = [
-        '2019-07-03T18:35:29.512364',
-        '2018-10-14T08:21:33.419441',
-        '2018-09-12T21:27:25.241689',
-        '2018-06-30T02:08:58.425572'
-    ]
-    assert [item['date'] for item in result] == expected_order
 
-def test_sort_asc():
-    result = sort_by_date(test_data, False)
-    expected_order = [
-        '2018-06-30T02:08:58.425572',
-        '2018-09-12T21:27:25.241689',
-        '2018-10-14T08:21:33.419441',
-        '2019-07-03T18:35:29.512364'
-    ]
-    assert [item['date'] for item in result] == expected_order
+def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
+    """
+    Сортирует список операций по дате
+
+    :param data: список словарей с операциями
+    :param descending: флаг сортировки по убыванию (True - убывание, False - возрастание)
+    :return: отсортированный список операций
+    """
+    # Для работы с датой будем использовать строковое сравнение
+    return sorted(
+        data,
+        key=lambda x: x['date'],
+        reverse=descending
